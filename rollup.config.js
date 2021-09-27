946f4ee7f3e9fcba5ac7a566c89a30d61b4b09c2
ivanvl01 <1v4nvl@gmail.com>
2021-09-27 16:30:15 +0300

    fix: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
-import json from '@rollup/plugin-json';
+import json from "@rollup/plugin-json";
 export default [
   {
     input: "src/index.js",
@@ -12,7 +12,7 @@ export default [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" }
     ],
-    plugins: [resolve(), commonjs(), babel(),json()]
+    plugins: [resolve(), commonjs(), babel(), json()]
   },
   {
     input: "src/index.js",
